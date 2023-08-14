3c90b073b7bf659235e0145da879277bb0a478ff
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-14 15:31:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ const outerRing = document.querySelector(".outer-ring");
 const innerWave = document.querySelector(".inner-wave");
 
 function updateVisualizer(amplitude) {
+  const dashOffset = 880 - amplitude * 800; // Adjust based on input
