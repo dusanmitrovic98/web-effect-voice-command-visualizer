02b3944743957bb264f7899e6796f6495f22adc1
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-14 15:32:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ const innerWave = document.querySelector(".inner-wave");
 function updateVisualizer(amplitude) {
   const dashOffset = 880 - amplitude * 800; // Adjust based on input
   outerRing.style.strokeDashoffset = dashOffset;
+  innerWave.style.opacity = amplitude;
